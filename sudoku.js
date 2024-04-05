60d7420baf072fb56c385f830e2862a207f8af57
Juri Aristov <juri.aristov@gmail.com>
2024-04-05 15:20:42 +0300

    Added fillFunction

--- sudoku.js
+++ sudoku.js
@@ -5,23 +5,47 @@ function read() {
 }
 
 function solve() {
-  let arr = [
+  const arr = [
     ["1", "-", "5"],
     ["8", "-", "2"],
     ["-", "-", "-"],
   ];
-  let etalon = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"],
-  ];
+  let etalon = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const test = arr.join(",").split(",");
+  const askArrSolved = test
+    .map((el) => {
+      if (el !== "-") {
+        etalon.splice(etalon.indexOf(el), 1);
+        return el;
+      } else {
+        return el;
+      }
+    })
+    .map((el) => {
+      if (el === "-") {
+        let index = Math.floor(Math.random() * etalon.length);
+        el = etalon[index];
+        etalon.splice(index, 1);
+        return el;
+      } else {
+        return el;
+      }
+    });
+  console.log(askArrSolved);
+
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
 }
 
+solve();
 function isSolved() {
+  let etalon = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+  ];
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
    * Возвращает булевое значение — решено это игровое поле или нет.
